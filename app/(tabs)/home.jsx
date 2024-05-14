41531c1a86c9aff200efcf8002a424af2b46a4b6
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2024-05-14 18:29:17 +0530

    main

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import { Image } from "react-native";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        // data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">Aura</Text>
+              </View>
 
-export default Home
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
 
-const styles = StyleSheet.create({})
+            {/* search Input */}
+            <SearchInput />
+
+            <View className="w-full  flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+              {/* the empty array for the input */}
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+            </View>
+          </View>
+        )}
+        // flatlist property
+
+        ListEmptyComponent={() => {
+          <Text className="text-white">empty</Text>;
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
